42316fd856c57a0188912d798ba79bcf2185487f
Jake Krogman <jacobkrogman92@gmail.com>
2016-03-15 18:19:14 -0400

    roughed out control flow of interpreter

--- jaz/Program.cs
+++ jaz/Program.cs
@@ -8,7 +8,7 @@ namespace jaz
 {
     public class JazInterpreter
     {
-        public static void main(String[] args)
+        public static void Main(String[] args)
         {
             if (!(args.Length > 0))
             {
@@ -18,9 +18,10 @@ namespace jaz
             // start parsing program into symbol table
             Parser parser = new Parser(args[0]);
             //evaluate symbol table once all symbols are loaded
-            JazEvaluator eval = new JazEvaluator();
+            //JazEvaluator eval = new JazEvaluator();
         }
     }
+
     public class SymbolTable
     {
         public static List<JazTuple<string, string>> symbolTable = new List<JazTuple<string, string>>();
@@ -128,7 +129,143 @@ namespace jaz
 
         public JazEvaluator()
         {
+            int lineNumber = 0;
+            JazTuple<string, string> currentInstruction = SymbolTable.symbolTable[lineNumber];
+            while(currentInstruction.getArgs() != "halt")
+            {
+                currentInstruction = SymbolTable.symbolTable[lineNumber];
+                switch (currentInstruction.getLabel())
+                {
+                    case "push":
+                        push(currentInstruction);
+                        break;
+                    case "rvalue":
+                        rValue(currentInstruction);
+                        break;
+                    case "lvalue":
+                        lValue(currentInstruction);
+                        break;
+                    case "pop":
+                        pop(currentInstruction);
+                        break;
+                    case ":=":
+                        colonEqual(currentInstruction);
+                        break;
+                    case "copy":
+                        copy(currentInstruction);
+                        break;
+                    case "label":
+                        label(currentInstruction);
+                        break;
+                    case "goto":
+                        goTo(currentInstruction);
+                        break;
+                    case "gofalse":
+                        goFalse(currentInstruction);
+                        break;
+                    case "gotrue":
+                        goTrue(currentInstruction);
+                        break;
+                    case "+":
+                        add(currentInstruction);
+                        break;
+                    case "-":
+                        subtract(currentInstruction);
+                        break;
+                    case "*":
+                        multiply(currentInstruction);
+                        break;
+                    case "/":
+                        divide(currentInstruction);
+                        break;
+                    case "div":
+                        mod(currentInstruction);
+                        break;
+                    case "&":
+                        and(currentInstruction);
+                        break;
+                    case "!":
+                        neg(currentInstruction);
+                        break;
+                    case "|":
+                        or(currentInstruction);
+                        break;
+                    case "<>":
+                        notEqual(currentInstruction);
+                        break;
+                    case "<=":
+                        lessThanEqualTo(currentInstruction);
+                        break;
+                    case ">=":
+                        greaterThanEualTo(currentInstruction);
+                        break;
+                    case "<":
+                        lessThan(currentInstruction);
+                        break;
+                    case ">":
+                        greaterThan(currentInstruction);
+                        break;
+                    case "=":
+                        equal(currentInstruction);
+                        break;
+                    case "print":
+                        print(currentInstruction);
+                        break;
+                    case "show":
+                        show(currentInstruction);
+                        break;
+                    case "begin":
+                        begin(currentInstruction);
+                        break;
+                    case "end":
+                        end(currentInstruction);
+                        break;
+                    case "return":
+                        ret(currentInstruction);
+                        break;
+                    case "call":
+                        call(currentInstruction);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+
+        private void push(JazTuple<string, string> instruction) { }
+        private void rValue(JazTuple<string, string> instruction) { }
+        private void lValue(JazTuple<string, string> instruction) { }
+        private void pop(JazTuple<string, string> instruction) { }
+        private void colonEqual(JazTuple<string, string> instruction) { }
+        private void copy(JazTuple<string, string> instruction) { }
+        private void label(JazTuple<string, string> instruction) { }
+        private void goTo(JazTuple<string, string> instruction) { }
+        private void goFalse(JazTuple<string, string> instruction) { }
+        private void goTrue(JazTuple<string, string> instruction) { }
+        private void halt(JazTuple<string, string> instruction) { }
+        private void add(JazTuple<string, string> instruction) { }
+        private void subtract(JazTuple<string, string> instruction) { }
+        private void multiply(JazTuple<string, string> instruction) { }
+        private void divide(JazTuple<string, string> instruction) { }
+        private void mod(JazTuple<string, string> instruction) { }
+        private void and(JazTuple<string, string> instruction) { }
+        private void neg(JazTuple<string, string> instruction) { }
+        private void or(JazTuple<string, string> instruction) { }
+        private void notEqual(JazTuple<string, string> instruction) { }
+        private void lessThanEqualTo(JazTuple<string, string> instruction) { }
+        private void greaterThanEualTo(JazTuple<string, string> instruction) { }
+        private void lessThan(JazTuple<string, string> instruction) { }
+        private void greaterThan(JazTuple<string, string> instruction) { }
+        private void equal(JazTuple<string, string> instruction) { }
+        private void print(JazTuple<string, string> instruction) { }
+        private void show(JazTuple<string, string> instruction)
+        {
+
+        }
+        private void begin(JazTuple<string, string> instruction) { }
+        private void end(JazTuple<string, string> instruction) { }
+        private void ret(JazTuple<string, string> instruction) { }
+        private void call(JazTuple<string, string> instruction) { }
     }
     
     public class Parser
@@ -137,10 +274,10 @@ namespace jaz
 
         public Parser(String file)
         {
-            this.parse(file);
+            this.parseFile(file);
         }
 
-        private void parse(string file)
+        private void parseFile(string file)
         {
             using (System.IO.StreamReader sr = System.IO.File.OpenText(file))
             {
@@ -155,19 +292,20 @@ namespace jaz
 
         private int parse(String t, int lineNumber) {
             t = t.Trim();
-            string head = t.Split(' ')[0].Trim();
-            string rest = t.Substring(t.Split(' ')[0].Length).Trim();
+            string label = t.Split(' ')[0].Trim();
+            string args = t.Substring(t.Split(' ')[0].Length).Trim();
 
-            if(!String.IsNullOrEmpty(head)) {
+            Console.WriteLine(label + " " + args);
+            if(!String.IsNullOrEmpty(label)) {
                 //load tuples into symbol table
-                if(head == "label")
+                if(label == "label")
                 {
-                    env.putLabel(rest, lineNumber);
-                    SymbolTable.symbolTable.Add(new JazTuple<string, string>(head, rest));
+                    env.putLabel(args, lineNumber);
+                    SymbolTable.symbolTable.Add(new JazTuple<string, string>(label, args));
                 }
                 else
                 {
-                    SymbolTable.symbolTable.Add(new JazTuple<string, string>(head, rest));
+                    SymbolTable.symbolTable.Add(new JazTuple<string, string>(label, args));
                 }
                 
                 lineNumber++;
@@ -175,8 +313,8 @@ namespace jaz
             return lineNumber;
         }
     }
-
-
+    
+    //A tuple is an instruction "label" and the arguments for that instruction
     public class JazTuple<Label, Args>
     {
         private Label L;
@@ -188,19 +326,20 @@ namespace jaz
             A = rest;
         }
 
-        public Label gethead() {
+        public Label getLabel() {
             return L;
         }
-        public Args getRest()
+        public Args getArgs()
         {
             return A;
         }
-        public void setHead(Label head)
+
+        public void setLabel(Label head)
         {
             L = head;
         }
 
-        public void setRest(Args rest)
+        public void setArgs(Args rest)
         {
             A = rest;
         }
